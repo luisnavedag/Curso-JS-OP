391bd5b222ff3e8a2561a1c9a3a089b4964ad6a0
luisnavedag <122492659+luisnavedag@users.noreply.github.com>
2023-02-04 21:39:30 -0300

    ejercicio listas

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/ejerciciolistas.js
@@ -0,0 +1,13 @@
+const lista = {
+    nombre: "Luis",
+    edad: 26,
+    desarrollador: true,
+    nacimiento: new Date(1996, 10, 08),
+    librofav: {
+        titulo: "Harry Potter 2",
+        autor: "J. K. Rowling",
+        fecha: 1998,
+        url: "https://www.amazon.com/-/es/J-K-Rowling/dp/8478884955"
+    } 
+}
+
---
+++ Curso-Javascript/mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,8 @@
+//Este es el mensaje de bienvenida//
+
+console.log("Esta es la puerta de entrada al proyecto")
+
+//Esta es la parte intermedia
+
+//Esta es la despedida//
+console.log("Adios")
