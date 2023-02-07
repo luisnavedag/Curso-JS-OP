eb57714ed205455ecd401a343a497c4b7c30bbd7
luisnavedag <122492659+luisnavedag@users.noreply.github.com>
2023-02-07 13:22:57 -0300

    ejercicio strings

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/ejercicio_strings.js
@@ -0,0 +1,20 @@
+let nombre = "Luis"
+let apellido = "Naveda"
+let estudiante = nombre + " " + apellido
+let estudianteMayus = estudiante.toUpperCase()
+let estudianteMinus = estudiante.toLowerCase()
+let cant = estudiante.length
+let primeraLetra = nombre.charAt(0)
+let ultimaLetra = apellido.charAt(5)
+let eliminaresp = estudiante.trim()
+let estaNombre = estudiante.includes(nombre)
+
+
+console.log(estudiante) 
+console.log(estudianteMayus)
+console.log(estudianteMinus)
+console.log(cant)
+console.log(primeraLetra)
+console.log(ultimaLetra)
+console.log(eliminaresp)
+console.log(estaNombre)
