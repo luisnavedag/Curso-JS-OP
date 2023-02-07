c3bfe121a81df958c8dad432be92d503a2255d9d
luisnavedag <122492659+luisnavedag@users.noreply.github.com>
2023-02-07 17:56:01 -0300

    ejercicio numbers

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/ejercicio_numbers.js
@@ -0,0 +1,14 @@
+let altura = parseInt(171)
+let alturaEnMetros = parseFloat(1.71)
+let pesoEnKg = parseFloat(75.3)
+let alturaRound = alturaEnMetros.toPrecision(1)
+let pesoRound = pesoEnKg.toPrecision(2)
+let mayorNumJs = "el máximo valor que se puede obtener en Javascript + 1 " + "=" + " " + Number.MAX_VALUE
+
+
+console.log(altura)
+console.log(alturaEnMetros)
+console.log(pesoEnKg)
+console.log(alturaRound)
+console.log(pesoRound)
+console.log(mayorNumJs)
