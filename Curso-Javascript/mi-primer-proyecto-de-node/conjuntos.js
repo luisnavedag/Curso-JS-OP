16f85f38f911831274e20587c48ecfd85411d45d
luisnavedag <122492659+luisnavedag@users.noreply.github.com>
2023-02-08 19:04:25 -0300

    creacion conjuntos.js

---
+++ Curso-Javascript/mi-primer-proyecto-de-node/conjuntos.js
@@ -0,0 +1,14 @@
+// Creando set de familia
+const familia = ["Luis", "Emi", "Deisy", "Alberto", "Deismar"];
+
+const mifamilia = new Set(familia);
+console.log(mifamilia)
+
+//Añadiendo mi nombre
+mifamilia.add("Luis");
+console.log(mifamilia)
+
+
+//Añadiendo Javascript
+mifamilia.add("Javascript");
+console.log(mifamilia)
